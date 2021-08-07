bbe6817bbd8ea5df67b16dd9616af75bb60e4a10
masafumi terazono <masafumi_t@0x48lab.com>
2021-08-07 15:31:17 +0900

    初回のプロジェクトを作った

---
+++ src/hikita/haruto/processing/blockbreaker/Main.java
@@ -0,0 +1,10 @@
+package hikita.haruto.processing.blockbreaker;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args){
+        PApplet.main(Main.class);
+    }
+}
