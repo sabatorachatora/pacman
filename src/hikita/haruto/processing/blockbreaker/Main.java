9e46c8d83c838692fbb6ee9d13bc86845c47462f
BuildTools <unconfigured@null.spigotmc.org>
2021-09-18 15:47:14 +0900

    てすと

--- src/hikita/haruto/processing/blockbreaker/Main.java
+++ src/hikita/haruto/processing/blockbreaker/Main.java
@@ -2,20 +2,8 @@ package hikita.haruto.processing.blockbreaker;
 
 import processing.core.PApplet;
 
-public class Main extends PApplet {
-
-    @Override
-    public void setup() {
-        super.setup();
-    }
-
-    @Override
-    public void draw() {
-        rect(14, 14, 14, 14);
-
-    }
-
-    public static void main(String[] args){
-        PApplet.main(Main.class);
+public class Main {
+    public static void main(String[] args) {
+        PApplet.main(ProcessingMain.class);
     }
 }
