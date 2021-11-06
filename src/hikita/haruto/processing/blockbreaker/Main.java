d29263946a9ce523292dfcd07dd4e08dca8b0f6e
masafumi terazono <hijirinhijirin@gmail.com>
2021-11-06 15:24:39 +0900

    余計なファイルを削除し、新規のプロジェクトの状態にしました

--- src/hikita/haruto/processing/blockbreaker/Main.java
+++
@@ -1,9 +0,0 @@
-package hikita.haruto.processing.blockbreaker;
-
-import processing.core.PApplet;
-
-public class Main {
-    public static void main(String[] args) {
-        PApplet.main(ProcessingMain.class);
-    }
-}
---
+++ src/hikita/haruto/processing/pacman/Main.java
@@ -0,0 +1,15 @@
+package hikita.haruto.processing.pacman;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    @Override
+    public void settings() {
+        size(500, 500);
+    }
+
+    public static void main(String[] args) {
+        PApplet.main(Main.class);
+    }
+}
